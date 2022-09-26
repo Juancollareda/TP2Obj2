0715bd60aadfda841ba41ec5902aee5729c15209
TomasMessiga <111101148+TomasMessiga@users.noreply.github.com>
2022-09-26 18:26:51 -0300

    Create Reserva.js

---
+++ Reserva.js
@@ -0,0 +1,21 @@
+const Empleado=require('./Empleado');
+
+function Reserva(nombre,apellido,dni,sueldo){
+    this.base=Empleado;
+    this.base(nombre,apellido,dni);
+    /** sueldo correspondiente a una hora de trabajo */
+    this.sueldo=sueldo;
+
+
+    this.pagarSueldo=function(horasTotales){
+        if (typeof horasTotales == 'number'){
+            if (horasTotales>0){
+                return (this.sueldo)*horasTotales;
+            } 
+        } else {
+            return 0;
+        }
+    };
+
+
+}
